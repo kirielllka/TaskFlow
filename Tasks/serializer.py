9238d06eeb24344a47f80f96c7cb65336e0c8b93
kirielllka <korotkevich3007@gmail.com>
2025-04-12 10:50:25 +0300

    feat:add Serializer, configure linter - ruff

--- Tasks/serializer.py
+++ Tasks/serializer.py
@@ -10,6 +10,33 @@ class UserSerializer(serializers.ModelSerializer):
 
 class TasksSerializer(serializers.ModelSerializer):
     author = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    category = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    repeat_days = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    class
+    category = serializers.HiddenField()
+    repeat_days = serializers.HiddenField()
+    author_info_display = serializers.SerializerMethodField()
+    category_display = serializers.SerializerMethodField()
+    repeat_days_display = serializers.SerializerMethodField()
+    class Meta:
+        model = Tasks
+        field = ['title','content','time','author_info_display','category_display','repeat_days_display','author','category','repeat_days']
+
+    def get_author_info_display(self):
+        serializer = UserSerializer(self.author)
+        return serializer.data
+
+    def get_repeat_days_display(self):
+        return self.repeat_days.split(' ')
+
+    def get_category_display(self):
+        serializer = CategoriesSerializer(self.category)
+        return serializer.data
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    created_user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    user_info_display = serializers.SerializerMethodField()
+    class Meta:
+        model = Categories
+        fields = ['category_name','user_info_display','created_user']
+
+    def get_user_info_display(self):
+        serializer = UserSerializer(self.created_user)
+        return serializer.data
