2bdd42f3fd7a1548bf93b58dbe8fca3ce271bbd2
kirielllka <korotkevich3007@gmail.com>
2025-04-12 11:07:50 +0300

    feat: add Serializer for models,add ViewSets, add and configure ruff linter

--- Tasks/views.py
+++ Tasks/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tasks, Categories
+from .serializer import UserSerializer,CategoriesSerializer,TasksSerializer
+
+from rest_framework.viewsets import ModelViewSet
+
+
+class TasksViewSet(ModelViewSet):
+    queryset = Tasks.objects.all()
+    serializer_class = TasksSerializer
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+
+    
