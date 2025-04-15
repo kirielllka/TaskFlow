0c7159de8ad26de9f2f9041c410b3f2a92277cdd
kirielllka <korotkevich3007@gmail.com>
2025-04-15 21:49:11 +0300

    feat(tests): add tests for category crud methods and merge get and create tests

--- Tasks/tests.py
+++ Tasks/tests.py
@@ -1,4 +1,5 @@
 import json
+from http.client import responses
 
 from django.contrib.auth.models import User
 from django.test import TestCase
@@ -22,17 +23,25 @@ class TasksTest(TestCase):
             "time": "08:00",
             "category": self.category,
             "repeat_days": "1 2 3",
-            "author":self.user
+            "author":self.user,
             }
     def test_tasks_create(self):
-        response = self.client.post(data=self.data_create,
+        response = self.client.post(data={
+            "title": "string",
+            "content": "string",
+            "time": "08:00",
+            "category": self.category.id,
+            "repeat_days": "1 2 3",
+            "author":self.user.id,
+            },
                                     path=self.url+"tasks/",
                                     content_type=self.content_type)
         self.assertEqual(response.status_code,201)
 
-    def test_tasks_get(self):
-        response = self.client.get(path=self.url+"tasks/")
-        self.assertEqual(response.status_code, 200)
+        if response.status_code == 201:
+            response = self.client.get(path=self.url+'tasks/')
+            self.assertEqual(response.status_code, 200)
+
 
     def test_tasks_put(self):
         data_changed = {
@@ -50,3 +59,40 @@ class TasksTest(TestCase):
         response = self.client.delete(path=self.url+f"tasks/{task.id}/", content_type=self.content_type)
         self.assertEqual(response.status_code, 204)
 
+@pytest.mark.django_db
+class CategoryTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username="test_user", password="12345qwerty")
+        self.client.force_login(user=self.user)
+        self.url = "/api/v1/"
+        self.content_type = "application/json"
+
+
+
+    def test_category_create(self):
+        data = {
+            "category_name":"test category",
+        }
+        response = self.client.post(path=self.url+f'category/',data=data,content_type=self.content_type)
+        self.assertEqual(response.status_code, 201)
+
+        if response.status_code == 201:
+            response = self.client.get(path=self.url+'category/')
+            self.assertEqual(response.status_code, 200)
+
+    def test_category_put(self):
+        changed_data = {
+            'category_name':'new data',
+            'created_user':self.user.id
+        }
+        category = Categories.objects.create(category_name = 'data', created_user=self.user)
+
+        response = self.client.put(path=self.url+f'category/{category.id}/', data=changed_data, content_type=self.content_type)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_delete(self):
+        category = Categories.objects.create(category_name = 'data', created_user=self.user)
+        response = self.client.delete(path=self.url+f'category/{category.id}/')
+        self.assertEqual(response.status_code, 204)
